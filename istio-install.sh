f262ce99b8fc20269be0642d1c6a0d120b6ffd62
Luana Rios <luarc99@gmail.com>
2022-05-04 19:18:41 -0300

    adicionada descrição das ações

--- istio-install.sh
+++ istio-install.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-# Instalação do Istio
-
+# Download
 curl -L https://istio.io/downloadIstio | sh -
+
+# Adicionando istioctl ao PATH
 cd istio-1.13.3
 echo $PWD
 export PATH=$PWD/bin:$PATH
+
+# Instalação
 istioctl install --set profile=demo -y
+
+# Habilitando Istio no namespace da aplicação (default)
 kubectl label namespace default istio-injection=enabled
+
 cd ..
 
