78f2c75ca43090ab21bc7a55f6bef3f9b5c645a0
Luana Rios <luarc99@gmail.com>
2022-05-06 13:31:23 -0300

    adicionado jaeger

--- dashboard.sh
+++ dashboard.sh
@@ -3,7 +3,7 @@
 main ()
 {
     echo -e "*** Escolha a aplicação de monitoramento ***\n"
-    echo -e "1-Grafana \n2-Kiali \n3-Prometheus\n"
+    echo -e "1-Grafana \n2-Kiali \n3-Prometheus \n4-Jaeger \n"
     read opcao;
     case $opcao in
         "1")
@@ -14,6 +14,9 @@ main ()
         ;;
         "3")
         prometheus
+        ;;
+        "4")
+        jaeger
         ;;
     esac
 }
@@ -31,6 +34,11 @@ prometheus()
 {
     echo "*** Iniciando dashboard Prometheus ***"
     istioctl dashboard prometheus
+}
+jaeger()
+{
+    echo "*** Iniciando dashboard Jaeger ***"
+    istioctl dashboard jaeger
 }
 
 main
